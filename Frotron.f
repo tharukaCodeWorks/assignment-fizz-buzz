114d57bb345f2a9e9850f071f0a6048f9ed25efe
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-10-22 04:21:34 +0530

    Frotran Unit Test

--- Frotron.f
+++ Frotron.f
@@ -10,4 +10,21 @@ Program FizzBuzz
             print*, n
         end if
    end do 
+
+    do
+        integer :: n
+        print *, "Enter the number: "
+        read *,n
+
+        if (mod(n,3)==0 .and. mod(n, 5)==0) then      
+            print*,  "Fizz Buzz" 
+        else if (mod(n, 5)==0) then       
+            print*,  "Buzz" 
+        else if (mod(n,3)==0) then 
+            print*,  "Fizz" 
+        else
+            print*, n
+        end if
+    end do  
+
 End Program FizzBuzz
